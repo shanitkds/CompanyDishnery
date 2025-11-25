abea677ea7a8cbcf013f099ad0ba78125ce22302
shanith2016 <mohammedshanith2016@gmail.com>
2025-11-25 10:10:55 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import CompanyMain from './Component/companyMain'
+function App() {
+
+  return (
+    <>
+     <div>
+      <CompanyMain/>
+     </div>
+    </>
+  )
+}
+
+export default App
